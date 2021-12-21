88ead61bfe9262f2f137b5e87af47fe0b6c9c8e8
PGarule <PGarule@fanatics.com>
2021-12-21 21:53:22 +0530

    log4j patch  - remove jndiLookup and jndiManager class from log4j-core

--- scripts/build_artifact.sh
+++ scripts/build_artifact.sh
@@ -24,4 +24,10 @@ cd artifacts
 
 cp -r ../front-end/dist dist
 tar -xf ../build/distributions/pulsar-manager.tar
+
+echo "remove JndiLookup.class from log4j-core"
+zip -q -d pulsar-manager/lib/log4j-core-*.jar org/apache/logging/log4j/core/lookup/JndiLookup.class && echo "removed JndiLookup.class"
+echo "remove JndiManager.class from log4j-core"
+zip -q -d pulsar-manager/lib/log4j-core-*.jar org/apache/logging/log4j/core/net/JndiManager.class && echo "removed JndiManager.class"
+
 tar -czvf pubsub-pulsar-manager-$version-bin.tar.gz pulsar-manager dist
