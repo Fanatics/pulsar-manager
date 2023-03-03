f5fff4d661b87f83a761119cdbd539e7e99d014f
Prabhudas Garule <pgarule@fanatics.com>
2023-03-03 18:56:45 +0530

    remove casdoor fronted

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -45,7 +45,7 @@ Object.keys(filters).forEach(key => {
 
 Vue.config.productionTip = false
 
-import Casdoor from 'casdoor-vue-sdk'
+// import Casdoor from 'casdoor-vue-sdk'
 import VueCompositionAPI from '@vue/composition-api'
 const config = {
   serverUrl: "http://localhost:7001",
@@ -55,7 +55,7 @@ const config = {
   redirectPath: "/#callback",
 };
 Vue.use(VueCompositionAPI)
-Vue.use(Casdoor,config)
+// Vue.use(Casdoor,config)
 new Vue({
   el: '#app',
   router,
