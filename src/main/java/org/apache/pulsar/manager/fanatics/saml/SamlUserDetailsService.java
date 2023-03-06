bb75af9e87aa032078afb33b929ed7b184d90b22
Prabhudas Garule <pgarule@fanatics.com>
2023-03-06 17:33:15 +0530

    add saml package

---
+++ src/main/java/org/apache/pulsar/manager/fanatics/saml/SamlUserDetailsService.java
@@ -0,0 +1,37 @@
+/**
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.pulsar.manager.fanatics.saml;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.saml.SAMLCredential;
+import org.springframework.security.saml.userdetails.SAMLUserDetailsService;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SamlUserDetailsService implements SAMLUserDetailsService {
+    @Override
+    public Object loadUserBySAML(SAMLCredential credential) throws UsernameNotFoundException {
+        String userID = credential.getNameID().getValue();
+        userID = userID.split("@")[0];
+        String [] authorities = credential.getAttributeAsStringArray("Groups");
+        List<GrantedAuthority> grantedAuthorities = Arrays.stream(authorities).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+        return new User(userID, "", true, true, true, true, grantedAuthorities);
+    }
+}
